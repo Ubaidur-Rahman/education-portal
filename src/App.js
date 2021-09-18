63e95e193c6186acdf49c61dc6449b88781c2b4c
raju10 <trolltube272@gmail.com>
2021-09-19 00:02:02 +0600

    test commit from arif

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import './App.css';
-import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>hellow i'm arif</h2>
     </div>
   );
 }
