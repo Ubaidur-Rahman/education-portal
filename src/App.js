11100fcf450019ae1f059330961568a64633f075
Ubaidur Rahman <ubaidurrahmanmcc@gmail.com>
2021-09-19 12:18:06 +0600

    testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Our New Project</p>
     </div>
   );
 }
